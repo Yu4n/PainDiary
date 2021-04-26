4b74ecf30add1e4f65ecf326b9081836a05c026e
Yu4n <28648731+yu4n@users.noreply.github.com>
2021-04-26 16:09:22 +0800

    interface

---
+++ app/src/main/java/com/example/paindiary/LoginActivity.java
@@ -0,0 +1,56 @@
+package com.example.paindiary;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity implements View.OnClickListener {
+    private static final String TAG = "login";
+    Button loginBtn = null;
+    EditText useridEt = null;
+    EditText passEt = null;
+    TextView promptText = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(this);
+        useridEt = (EditText) findViewById(R.id.userId);
+        passEt = (EditText) findViewById(R.id.pass);
+        promptText = (TextView) findViewById(R.id.promptText);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        String userid = useridEt.getText().toString().trim();
+        String pass = passEt.getText().toString().trim();
+        if (userid.equals("")) {
+            promptText.setText(R.string.userIdError);
+            return;
+        }
+        if (pass.equals("")) {
+            promptText.setText(R.string.passError);
+            return;
+        }
+
+
+        if (userid.equals("admin") && pass.equals("admin")) {
+            Toast.makeText(this, R.string.loginSuccess, Toast.LENGTH_LONG).show();
+            Intent intent_hello = new Intent(this, MainActivity.class); // todo create a login class
+            startActivity(intent_hello);
+            LoginActivity.this.finish();
+        } else {
+            Toast.makeText(this, R.string.loginError, Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
--- app/src/main/java/com/example/paindiary/MainActivity.java
+++ app/src/main/java/com/example/paindiary/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.paindiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginBtn = (Button) findViewById(R.id.signin);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+        Button regBtn = (Button) findViewById(R.id.signup);
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+            }
+        });
     }
 }
