11a6b5dd1a0ae26594acc86df5b74bbc6685d8a7
Yu4n <28648731+yu4n@users.noreply.github.com>
2021-04-29 13:57:18 +0800

    weather home

--- app/src/main/java/com/example/paindiary/HomeActivity.java
+++ app/src/main/java/com/example/paindiary/HomeActivity.java
@@ -1,14 +1,79 @@
 package com.example.paindiary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class HomeActivity extends AppCompatActivity {
+
+    public static String BaseUrl = "http://api.openweathermap.org/";
+    public static String AppId = "d7c33b70b706590fe2eb5d2a1274a71b";
+    public static String lat = "31.3114";
+    public static String lon = "120.6181";
+
+    private TextView weatherData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        weatherData = findViewById(R.id.weather_details);
+
+
+        findViewById(R.id.getWeather).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getCurrentData();
+            }
+        });
     }
+    void getCurrentData() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BaseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        WeatherService service = retrofit.create(WeatherService.class);
+        Call<WeatherResponse> call = service.getCurrentWeatherData(lat, lon, AppId);
+        call.enqueue(new Callback<WeatherResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<WeatherResponse> call, @NonNull Response<WeatherResponse> response) {
+                if (response.code() == 200) {
+                    WeatherResponse weatherResponse = response.body();
+                    assert weatherResponse != null;
+
+                    String stringBuilder = "Country: " +
+                            weatherResponse.sys.country +
+                            "\n" +
+                            "Temperature: " +
+                            (weatherResponse.main.temp - 273) +
+                            "\n" +
+                            "Humidity: " +
+                            weatherResponse.main.humidity +
+                            "\n" +
+                            "Pressure: " +
+                            weatherResponse.main.pressure;
+
+                    weatherData.setText(stringBuilder);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<WeatherResponse> call, @NonNull Throwable t) {
+                weatherData.setText(t.getMessage());
+            }
+        });
+    }
+
+
 }
